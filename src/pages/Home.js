d43997610d27a94e26f1d883de6d592f56c1ed42
tilak-13 <67039398+tilak-13@users.noreply.github.com>
2023-01-30 23:28:52 +0545

    improvement in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,26 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-
-import AuthContext from "../context/AuthContext";
+import './Home.css'
 
 const Home = () => {
-    const { user } = useContext(AuthContext);
 
   return (
-    <div className="container">
-      <div className="jumbotron mt-5">
-        {user && <h1 className="display-4">Welcome {user.name}</h1>}
-
-        {!user && (
-          <Link className="btn btn-primary btn-lg" to="/login" role="button">
-            Login
-          </Link>
-        )}
-      </div>
+    <div>
+        <div className="header-main">
+          <h1 className="header-title">
+              <p>FIND YOUR </p><p className="span">PERFECT</p><p> JOB EASILY</p>
+          </h1>
+        </div>
+        <div className="search-wrapper">
+            <div className="search-box">
+                <div className="search-card">
+                    <input type="text" className="search-input" placeholder="Job Title or Keywords"/>
+                    <button className="search-button">Search</button>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
 
 export default Home;
+
+
