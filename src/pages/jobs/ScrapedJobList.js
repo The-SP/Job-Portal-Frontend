0939e95f3cc0eb101bdda7973b7c898ef68c0f14
tilak-13 <67039398+tilak-13@users.noreply.github.com>
2023-02-06 18:39:11 +0545

    search box added in scraped job

--- src/pages/jobs/ScrapedJobList.js
+++ src/pages/jobs/ScrapedJobList.js
@@ -5,9 +5,11 @@ import Pagination from "./Pagination";
 import { urls } from "../../config";
 
 const ScrapedJobList = () => {
-  const [jobs, setJobs] = useState(null);
-  const [currentPage,setCurrentPage]=useState(1);
-  const jobsPerPage=10
+  const [jobs, setJobs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [jobsPerPage] = useState(10);
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     axiosInstance
@@ -15,27 +17,57 @@ const ScrapedJobList = () => {
       .then((res) => {
         console.log("Total jobs fetched:", (res.data).length);
         setJobs(res.data);
+        setFilteredJobs(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  //Filter the jobs by searchTerm
+  // useEffect(() => {
+  //   setFilteredJobs(
+  //     jobs.filter((job) => job.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  //     job.company.toLowerCase().includes(searchTerm.toLowerCase())
+  //     ) 
+  //   );
+  // }, [searchTerm]);
+
+  const handleSearch = ()=>{
+    setFilteredJobs(
+      jobs.filter((job) => job.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      job.company.toLowerCase().includes(searchTerm.toLowerCase())
+      ) 
+    );
+  }
+
   //Get current posts
   const indexofLastJob = currentPage * jobsPerPage;
-  const indexofFirstJob= indexofLastJob - jobsPerPage;
-  const currentJobs = jobs? jobs.slice(indexofFirstJob,indexofLastJob):[];
+  const indexofFirstJob = indexofLastJob - jobsPerPage;
+  const currentJobs = filteredJobs?filteredJobs.slice(indexofFirstJob, indexofLastJob):[];
 
   //change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  //Handle search input change
+  const handleSearchChange = (event) => setSearchTerm(event.target.value);
 
   if (!jobs) return <></>;
 
   return (
     <div className="container-fluid py-5 px-5">
       <h2 className="text-center mb-5">Scraped Jobs</h2>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Enter Job Title or Company Name"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       {currentJobs.map((job, index) => {
         return <ScrapedJobItem key={index} job={job} />;
       })}
-      <Pagination jobsPerPage={jobsPerPage} totalJobs={jobs.length} paginate={paginate}/>
+      <Pagination jobsPerPage={jobsPerPage} totalJobs={filteredJobs.length} paginate={paginate} />
     </div>
   );
 };
