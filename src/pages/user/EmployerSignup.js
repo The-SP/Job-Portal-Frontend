c99a147880487c16bf7bb6f86b2990ea118ccd63
tilak-13 <67039398+tilak-13@users.noreply.github.com>
2023-02-21 22:28:53 +0545

    modified employer sign up

--- src/pages/user/EmployerSignup.js
+++ src/pages/user/EmployerSignup.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axiosInstance from "../../axios_instance";
 import { auth_urls } from "../../config";
+import signupSvg from './signup.svg'
+import './user.css'
 
 const EmployerSignup = () => {
   const navigate = useNavigate();
@@ -47,19 +49,35 @@ const EmployerSignup = () => {
 
   return (
     <section>
-      <div className="container vh-100">
+      <div className="main-container1 container-fluid">
         <div className="row d-flex justify-content-center align-items-center py-2">
-          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div className="panel col-6 px-0">
+            <div className="description">
+              <div className="mb-4">
+                <p>
+                  Already have an account?{" "}
+                </p>
+                <button className="btn btn-outline-dark btn-lg px-5 text-white fw-bold"
+                 onClick={()=> {navigate('/login')}}>
+                    Login
+                </button>
+              </div>
+              <div>
+                <img src={signupSvg} alt="Not Found" />
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5 px-0 mx-auto">
             <div
-              className="card bg-dark text-white"
-              style={{ borderRadius: 1 + "rem" }}
+              className="signin bg-light text-black"
+              style={{ borderRadius: 0.5 + "rem" }}
             >
-              <div className="card-body p-5 text-center">
-                <div className="pb-5">
+              <div className="card-body p-4 text-center">
+                <div className="pb-3">
                   <h2 className="fw-bold mb-2 text-uppercase">
                     Employer Sign Up
                   </h2>
-                  <p className="text-white-50 mb-5">
+                  <p className="text-black-50 mb-5">
                     Please enter your login and password!
                   </p>
 
@@ -77,7 +95,7 @@ const EmployerSignup = () => {
                     </label>
                   </div>
 
-                  <div className="form-outline form-white mb-4">
+                  <div className="form-outline form-black mb-3">
                     <input
                       type="text"
                       id="typeUsernameX"
@@ -86,11 +104,11 @@ const EmployerSignup = () => {
                       onChange={(e) => setName(e.target.value)}
                     />
                     <label className="form-label" htmlFor="typeUsernameX">
-                      Company
+                      Company Name
                     </label>
                   </div>
 
-                  <div className="form-outline form-white mb-4">
+                  <div className="form-outline form-black mb-3">
                     <input
                       type="password"
                       id="typePasswordX"
@@ -103,24 +121,15 @@ const EmployerSignup = () => {
                     </label>
                   </div>
 
-                  <div className="error text-danger fw-bold mb-4">{error}</div>
+                  <div className="error text-danger fw-bold mb-3">{error}</div>
 
                   <button
-                    className="btn btn-outline-light btn-lg px-5"
+                    className="btn btn-outline-dark btn-lg px-5"
                     type="submit"
                     onClick={handleSubmit}
                   >
                     Sign Up
                   </button>
-                </div>
-
-                <div>
-                  <p className="mb-0">
-                    Already have an account?{" "}
-                    <Link to="/login" className="text-white-50 fw-bold">
-                      Login
-                    </Link>
-                  </p>
                 </div>
               </div>
             </div>
